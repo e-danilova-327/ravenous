9708c85849208d1eaee8a3f47769b7c89c1e154c
Elena <e.cotar327@gmail.com>
2021-09-04 18:22:36 +0300

    A Yelp-clone app called Ravenous made by using React

---
+++ ravenous/src/util/Yelp.js
@@ -0,0 +1,38 @@
+const apiKey =
+    'arGzhoj1fPubN0LNekxdQe079MXDnCvvvyZeEA8eESN0qXnBGkzf22A_DZexjuy06dE2Hce13m6YVNZuW56e5qbbUSFVBoSO_pA-CzsqhptmNQ7sbtagmR8Uw30zYXYx';
+
+const Yelp = {
+    searchYelp(term, location, sortBy) {
+        return fetch(
+            `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${apiKey}`,
+                },
+            }
+        )
+            .then((response) => {
+                return response.json();
+            })
+            .then((jsonResponse) => {
+                if (jsonResponse.businesses) {
+                    return jsonResponse.businesses.map((business) => {
+                        return {
+                            id: business.id,
+                            imageSrc: business.image_url,
+                            name: business.name,
+                            address: business.location.address1,
+                            city: business.location.city,
+                            state: business.location.state,
+                            zipCode: business.location.zip_code,
+                            category: business.categories[0].title,
+                            rating: business.rating,
+                            reviewCount: business.review_count,
+                        };
+                    });
+                }
+            });
+    },
+};
+
+export default Yelp;
